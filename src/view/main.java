32d435cc99e7cb0c3010fcafcc714bdeb1249314
a <a>
2019-10-12 10:11:20 +0200

    Working Suite

--- src/view/main.java
+++ src/view/main.java
@@ -1,7 +1,6 @@
 package view;
 
 import Objects.HitMiss;
-import Objects.Word;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,17 +9,17 @@ public class main {
 
     public static void main(String[] args) {
         try {
-            HitMiss word= new HitMiss("thing", false);
+            HitMiss word = new HitMiss("thing", false);
             System.out.println(word.exist());
             System.out.println(word.insert());
             System.out.println(word.exist());
             System.out.println(word.delete());
             System.out.println(word.exist());
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
+
 }
